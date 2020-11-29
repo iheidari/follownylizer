d7579d46f412720e2e2615cfdeb8d231944fe3d6
iheidari <iman.heydari@gmail.com>
2020-11-28 18:47:11 -0800

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          variant="outlined"
+          color="primary"
+          size="large"
+          style={{ borderColor: "white", color: "white" }}
         >
-          Learn React
-        </a>
+          <InstagramIcon style={{ marginRight: "10px" }} />
+          Login
+        </Button>
       </header>
     </div>
   );
