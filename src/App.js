b267ab7f5be0072229212b742761644cfef96f3e
iheidari <iman.heydari@gmail.com>
2020-11-28 20:02:44 -0800

    add instagram login link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Button from "@material-ui/core/Button";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import "./App.css";
+import { CLIENT_ID, REDIRECT_URI } from "./config";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           color="primary"
           size="large"
           style={{ borderColor: "white", color: "white" }}
+          href={`https://www.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`}
         >
           <InstagramIcon style={{ marginRight: "10px" }} />
           Login
